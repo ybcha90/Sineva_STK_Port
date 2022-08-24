6bb73a12b36fa036196024022dce6c196b7820c6
ChaYoungBin <cha154198@naver.com>
2022-08-24 10:19:23 +0800

    Safety Check Thread 추가

---
+++ Sineve_STK_Port/Checker/SafetyChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sineva_STK_Port.Checker
+{
+    public class SafetyChecker
+    {
+        public void CheckStart()
+        {
+            Task.Run(() => Run());
+        }
+
+        private void Run()
+        {
+            try
+            {
+                while (true)
+                {
+
+                    Thread.Sleep(1);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
