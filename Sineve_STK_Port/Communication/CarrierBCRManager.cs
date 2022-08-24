00e5772bf2521e923db8641d16e646c1316a5226
ChaYoungBin <cha154198@naver.com>
2022-08-24 10:19:40 +0800

    Carrier BCR Reader Class 추가

--- Sineve_STK_Port/Communication/CarrierBCRManager.cs
+++ Sineve_STK_Port/Communication/CarrierBCRManager.cs
@@ -8,6 +8,73 @@ namespace Sineva_STK_Port.Communication
 {
     public class CarrierBCRManager
     {
+        #region Singleton
+        private static volatile CarrierBCRManager instance;
+        private static object syncRoot = new object();
+
+        public static CarrierBCRManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new CarrierBCRManager();
+                    }
+                }
+                return instance;
+            }
+        }
+        #endregion
+
+        private bool IsConnect { get; set; }
+        private string CarrierID { get; set; }
+
+        public bool ConnectCarrierReader()
+        {
+            bool isConnect = false;
+
+            IsConnect = IsConnect;
+            return isConnect;
+        }
+
+        public bool IsCarrierReaderConnect()
+        {
+            return IsConnect;
+        }
+
+        public (bool, string) ReadCarrierID()
+        {
+            try
+            {
+                string carrierID = string.Empty;
+                bool isReadComplete = false;
+                Task.Run(() => CarrierIDReadStart());
+                return (isReadComplete, carrierID);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());   
+                return (false, ex.ToString());     
+            }
+        }
             
+        private void CarrierIDReadStart()
+        {
+            try
+            {
+                while (true)
+                {
+
+                    Thread.Sleep(10);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());   
+            }
+        }
     }
 }
