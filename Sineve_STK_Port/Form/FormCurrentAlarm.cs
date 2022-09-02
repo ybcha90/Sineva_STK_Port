7fffb7b6a83e17b097c65005248246648c123308
guohong007 <lin47106@163.com>
2022-09-02 10:42:46 +0800

    CurrentForm 추가

---
+++ Sineve_STK_Port/Form/FormCurrentAlarm.cs
@@ -0,0 +1,26 @@
+﻿using Sineva_STK_Port.Management;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sineva_STK_Port
+{
+    public partial class FormCurrentAlarm : Form
+    {
+        public FormCurrentAlarm()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCurrentAlarm_Load(object sender, EventArgs e)
+        {
+            //DataTable Portinfo = DBManager.Instance.GetCurrentAlarm();
+        }
+    }
+}
